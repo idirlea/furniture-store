a1099b829012cfb92dc5605ff8008ea586c257ff
Ionut Andrei Dirlea <ionut.dirlea@gmail.com>
2021-03-05 21:32:02 +0200

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '14152c74237a4dd65110986957ba76bd'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '81ba6e08-98e7-41d2-8af0-dcd17c772f78'
+};
